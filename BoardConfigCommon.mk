e1ecf179594663fc6d1339827dfe74a38686d4b6
Nick <nickgunale@gmail.com>
2018-06-19 00:50:37 +0530

    msm8956-common: remove dex optimizations for now
    Revert "msm8956-common: Enable boot and system server dex-preopt"
    
    This reverts commit ba5e5ca9cdfbcd627b14857913981c71346c3d76.
    
    Revert "msm8956-common: optimize dexpreopt"
    
    This reverts commit 9e98621dcd029fa55a81dac4e064aeec60f99b14.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,12 +111,12 @@ BOARD_USES_QCNE := true
 # Dex
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT_DEBUG_INFO := false
-    USE_DEX2OAT_DEBUG := false
-    DONT_DEXPREOPT_PREBUILTS := true
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
   endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 
 # Display
 MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
